434a28b6e4d032d0b54f8ff65048d15b503bbc91
arshathunnisa <129770838+arshathunnisa@users.noreply.github.com>
2023-05-08 19:40:22 +0530

    upload afile

--- arshath_assignment7.py
+++ arshath_assignment7.py
@@ -6,28 +6,28 @@ This is a temporary script file.
 """
 def credit_card_offer(salary,credit_card_limit):
     if salary<10000:
-        credit_limit=salary*10
+        credit_limit=salary*.10
     elif salary>=10000 and salary<=30000:
-        credit_limit=salary*30
+        credit_limit=salary*.30
         return credit_limit
     credit_card_offer(35000,2)
     
     def dmart_discount_offer(purchase_amount,discount):
         if purchase_amount<20000:
-            discount="20%"
+            discount=0.2
         elif purchase_amount>=20000 and purchase_amount<=40000:
-            discount="30%"
+            discount=0.3
         elif purchase_amount>50000:
-            discount="40%"
+            discount=0.4
             return discount
         
         def amazon_offer(product_type,discount):
             if product_type=="electronic":
-                discount="20%"
+                discount=0.2
             elif product_type=="cloth":
-                discount="30%"
+                discount=0.3
             elif product_type=="footware":
-                discount="40%"
+                discount=0.4
                 return discount
 amazon_offer("footware",1)
         
